f42d3a808b4dae80073797040225251c41699073
Wicki <kotwicki1990@gmail.com>
2015-10-25 12:42:19 -0400

    Adding starting files for App

---
+++ appRoute.js
@@ -0,0 +1,32 @@
+   (function () {
+    'use strict';
+ 
+    var app= angular.module('bsApp');
+        app.config(function($routeProvider) {
+        $routeProvider
+
+            .when('/', {
+                templateUrl : 'pages/lobby.html',
+                controller  : 'lobbyCtrl'
+            })
+
+            .when('/account', {
+                templateUrl : 'pages/account.html',
+                controller  : 'accountCtrl'
+            })
+
+            .when('/help', {
+                templateUrl : 'pages/help.html',
+                controller  : 'helpCtrl'
+            })
+         .when('/login', {
+                templateUrl : 'pages/login.html',
+                controller  : 'loginCtrl'
+            })
+         .when('/stats', {
+                templateUrl : 'pages/stats.html',
+                controller  : 'statsCtrl'
+            });
+       });
+ 
+}());
